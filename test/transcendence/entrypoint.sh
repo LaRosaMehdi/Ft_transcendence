a11074c963218d76ea2da0359574be698bb122c9
Mehdi La rosa <mla-rosa@c1r1p1.42nice.fr>
2024-03-07 17:38:39 +0100

    First step

---
+++ test/transcendence/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Wait for the database service to become available
+until pg_isready -U mehdi -d ft_transcendence_database -h ft_transcendence_database -p 5432 -q; do
+  echo "Waiting for the database service to become available..."
+  sleep 1
+done
+
+# Generate random numbers and fill a vector (example)
+# Insert your code here
+#Echo creating app_folder
+
+
+
+
+
+# Start the Django development server or any other necessary commands
+#python /usr/src/app/app/manage.py createsuperuser
+#echo "APRES CREATE USER"
+#echo "APRES start api"
+#python -m pip install Pillow
+#echo "APRES install pillow"
+python /usr/src/app/app/manage.py migrate
+#echo "APRES migrate"
+#python ./app/manage.py collectstatic --no-input
+#echo "APRES STATIC"
+exec python manage.py runserver 0.0.0.0:443
+#echo "APRES run server"
--- test/transcendence/transcendence/settings.py
+++ test/transcendence/transcendence/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'transcendence.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'ft_transcendence_database'),
+        'USER': os.environ.get('DB_USER', 'mehdi'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'melody'),
+        'HOST': os.environ.get('DB_HOST', 'ft_transcendence_database'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
