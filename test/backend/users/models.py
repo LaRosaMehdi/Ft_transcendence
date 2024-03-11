9c425501e068ca78ae77161b7a8fbe257c616b3d
Mel <melody@42nice.fr>
2024-03-11 14:44:27 +0100

    Fixe admin visualization of user table

--- test/backend/users/models.py
+++ test/backend/users/models.py
@@ -2,9 +2,13 @@
 
 from django.db import models
 
+# @admin.register(user)
 class user(models.Model):
     username = models.CharField(max_length=100)
     email = models.EmailField()
     password = models.CharField(max_length=100, null=True)
     #created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{ self.username } - { self.email }"
     
