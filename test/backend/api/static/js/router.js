3e9734decb5974528e80dac8e2462289ef3c0d42
Dorian Janninelle <dojannin@c4r2p6.42nice.fr>
2024-03-29 20:07:38 +0100

    FrontPersoDebut

--- test/backend/api/static/js/router.js
+++ test/backend/api/static/js/router.js
@@ -14,19 +14,36 @@ function loadPersoPage() {
 
 
 function loadProfileDataJson() {
-    $.ajax({
-        url: 'api/generate_profile_json/',
-        type: 'GET',
-        success: function(data) {
-            var profileDiv = $('<div>').addClass('profile-section');
-            profileDiv.append('<h3>Username</h3><div class="profile-data">' + data.username + '</div>');
-            profileDiv.append('<h3>Email</h3><div class="profile-data">' + data.email + '</div>');
-            profileDiv.append('<h3>Age</h3><div class="profile-data">' + data.age + '</div>');
-            $('#profileData').html('<pre>' + JSON.stringify(data, null, 2) + '</pre>');
-        },
-        error: function(xhr, status, error) {
-            console.error(error);
-        }
-    });
+    var profileData = $('#profileData');
+
+    // Vérifier si le profil est déjà affiché
+    if (profileData.is(':visible')) {
+        // Si c'est le cas, le cacher
+        profileData.hide();
+    } else {
+        $.ajax({
+            url: 'api/generate_profile_json/',
+            type: 'GET',
+            success: function(data) {
+                var profileDiv = $('<div>').addClass('profile-section');
+
+                Object.keys(data).forEach(function(key) {
+                    // Créer un compartiment pour chaque propriété
+                    var propertyDiv = $('<div>').addClass('profile-property');
+                    propertyDiv.append('<span class="property-label">' + key + ': </span>');
+                    propertyDiv.append('<span class="property-value">' + data[key] + '</span>');
+                    profileDiv.append(propertyDiv);
+                });
+                // profileDiv.append('<h3>Username</h3><div class="profile-data">' + data.username + '</div>');
+                // profileDiv.append('<h3>Email</h3><div class="profile-data">' + data.email + '</div>');
+                // profileDiv.append('<h3>Age</h3><div class="profile-data">' + data.age + '</div>');
+                $('#profileData').html(profileDiv);
+                $('#profileData').show();
+            },
+            error: function(xhr, status, error) {
+                console.error(error);
+            }
+        });
+    }
 }
 
